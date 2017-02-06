2144c3945f369458e43202225447bbdb5c8267bc
NetLamp <oplpublishergerman@gmail.com>
2017-02-06 10:37:46 +0100

    ignore(Class) Method

--- src/io/github/coalangsoft/dragdropfx/DragDropFX.java
+++ src/io/github/coalangsoft/dragdropfx/DragDropFX.java
@@ -1,5 +1,7 @@
 package io.github.coalangsoft.dragdropfx;
 
+import java.util.ArrayList;
+
 import io.github.coalangsoft.lib.data.Func;
 import io.github.coalangsoft.visit.Visitor;
 import io.github.coalangsoft.visit.VisitorInfo;
@@ -16,12 +18,15 @@ import javafx.scene.media.MediaView;
 @VisitorInfo(Node.class)
 public class DragDropFX extends Visitor{
 	
+	private ArrayList<Class<?>> ignore;
+
 	{
 		addFunction(TabPane.class, new TabPaneContentVisitor(this));
 		addFunction(Parent.class, new ParentChildrenVisitor(this));
 		addFunction(TextInputControl.class, new Func<Object, Void>() {
 			@Override
 			public Void call(Object p) {
+				if(isIgnored(p)){return null;}
 				TextInputControl c = (TextInputControl) p;
 				DnDPrepare.textinput(c);
 				return null;
@@ -30,6 +35,7 @@ public class DragDropFX extends Visitor{
 		addFunction(Labeled.class, new Func<Object, Void>() {
 			@Override
 			public Void call(Object p) {
+				if(isIgnored(p)){return null;}
 				Labeled c = (Labeled) p;
 				DnDPrepare.labeled(c);
 				return null;
@@ -38,6 +44,7 @@ public class DragDropFX extends Visitor{
 		addFunction(ImageView.class, new Func<Object, Void>(){
 			@Override
 			public Void call(Object p) {
+				if(isIgnored(p)){return null;}
 				ImageView c = (ImageView) p;
 				DnDPrepare.imageview(c);
 				return null;
@@ -46,11 +53,40 @@ public class DragDropFX extends Visitor{
 		addFunction(MediaView.class, new Func<Object, Void>(){
 			@Override
 			public Void call(Object p) {
+				if(isIgnored(p)){return null;}
 				MediaView c = (MediaView) p;
 				DnDPrepare.mediaview(c);
 				return null;
 			}
 		});
+		
+		ignore = new ArrayList<Class<?>>();
+	}
+	
+	public void ignoreType(Object o){
+		if(o instanceof Class){
+			ignore.add((Class<?>) o);
+		}else{
+			try {
+				Class<?> coaReflect = Class.forName("io.github.coalangsoft.reflect.Clss");
+				if(coaReflect.isInstance(o)){
+					ignore.add(
+						(Class<?>) coaReflect.getField("base").get(o)
+					);
+				}
+			} catch (ClassNotFoundException | IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+				throw new RuntimeException("Unexpected ignore type description: " + o);
+			}
+		}
+	}
+	
+	private boolean isIgnored(Object p) {
+		for(int i = 0; i < ignore.size(); i++){
+			if(ignore.get(i).isInstance(p)){
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
