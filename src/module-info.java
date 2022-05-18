ff8519989509b72c0ccc9cc56b2c434fbcc00cc2
NetLamp-Y <oplpublishergerman@gmail.com>
2022-05-18 22:38:22 +0200

    Create module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module dragdropfx {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.media;
+    requires java.desktop;
+    requires javafx.swing;
+    exports io.github.coalangsoft.dragdropfx;
+    exports io.github.coalangsoft.lib.data;
+}
